60426729f5c5cf74f5d036ee455118dfa47cf133
gusalbukrk <gusalbukrk@gmail.com>
2024-02-03 23:27:43 -0300

    add webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
+import HelloWorldPanel from './panels/HelloWorldPanel';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "extension" is now active!');
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from extension!');
+		// vscode.window.showInformationMessage('Hello World from extension!');
+		HelloWorldPanel.render();
 	}));
 }
 
